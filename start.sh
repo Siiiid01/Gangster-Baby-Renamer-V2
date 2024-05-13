666ab3faff53c7a6cdcb6752a142196942e765e9
Siiiid01 <153982728+Siiiid01@users.noreply.github.com>
2024-05-13 16:07:34 +0530

    Update

---
+++ start.sh
@@ -0,0 +1,5 @@
+echo "Cloning Repo...."
+git clone https://github.com/Siiiid01/Gangster-Baby-Renamer-V2 /cd /Gangster-Baby-Renamer-V2
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
